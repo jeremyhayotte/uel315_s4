9f536b8a394fd91d51c3526e035fa2ad9a58709d
jeremyhayotte <jeremy.hayotte@etu.unilim.fr>
2023-01-29 18:36:18 +0100

    Mise à jour de create et update + changement du type de l'id sur string

--- src/users/dto/create-user.input.ts
+++ src/users/dto/create-user.input.ts
@@ -1,7 +1,33 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { InputType, Int, Field, ObjectType, ID } from '@nestjs/graphql';
 
-@InputType()
+/*@InputType()
 export class CreateUserInput {
   @Field(() => Int, { description: 'Example field (placeholder)' })
   exampleField: number;
+}*/
+
+@InputType()
+export class CreateUserInput {
+    @Field()
+    firstname: string;
+    @Field()
+    lastname: string;
+    @Field()
+    email: string;
+    @Field()
+    location: string;
+}
+
+@ObjectType()
+export class CreateUserDto {
+    @Field(() => ID)
+    id: string;
+    @Field()
+    readonly firstname: string;
+    @Field()
+    readonly lastname: string;
+    @Field()
+    readonly email: string;
+    @Field()
+    readonly location: string;
 }
